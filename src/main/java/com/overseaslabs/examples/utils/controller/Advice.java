f571e774dd83b771508955312e2dc085dc795c2a
Sergey Vaschenko <vaschenko.sergey@gmail.com>
2018-09-07 09:31:26 +0300

    add controller advice --force

---
+++ src/main/java/com/overseaslabs/examples/utils/controller/Advice.java
@@ -0,0 +1,26 @@
+package com.overseaslabs.examples.utils.controller;
+
+import com.overseaslabs.examples.utils.exception.ResourceConflictException;
+import com.overseaslabs.examples.utils.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class Advice {
+    @ResponseBody
+    @ExceptionHandler(ResourceConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    String resourceConflictsHandler(ResourceConflictException e) {
+        return e.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String resourceNotFoundHandler(ResourceNotFoundException e) {
+        return e.getMessage();
+    }
+}
